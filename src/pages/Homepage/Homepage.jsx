f928941991183f2180b5ac5e17ce0a3c531d50d5
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-07-19 20:52:58 +0200

    banner implamentation

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from '../../components/Banner/Banner'
 import './Homepage.css'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      <Banner headline={"Find student homes with bills included"}
+             subhead={"A simple and faster way to search for student accomodations"} 
+        />
+    </div>
   )
 }
 
