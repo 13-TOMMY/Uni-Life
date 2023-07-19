6f5506896c3975f706c214d5a1b45787c790e5dd
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-07-19 20:18:34 +0200

    footer completed, need to do media quries

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { FaRegCopyright } from "react-icons/fa";
+import KeepInTouch from '../KeepInTouch/KeepInTouch'
 import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <KeepInTouch />
+      <div className="footer">
+          <div className="footer-left">
+              <p>About Us</p>
+              <p>Terms & Conditions</p>
+              <p>Privacy & Cookies Policies</p>
+          </div>
+          <div className="footer-right">
+              <p>2023</p>
+                <div className='copyright-uniLife'>
+                  <FaRegCopyright />
+                <p>UniLife</p>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
