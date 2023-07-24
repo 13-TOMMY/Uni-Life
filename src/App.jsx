ee5e42cec8f26cc87d24440cb18185062b1726af
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-07-24 21:27:42 +0200

    global context

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,22 @@ import CityDetailsPage from './pages/CityDetailsPage/CityDetailsPage';
 import HomeDetailPage from './pages/HomeDetailPage/HomeDetailPage';
 import './App.css';
 import Favorites from './pages/Favorites/Favorites';
+import FavContextProvider from './contexts/FavContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/SeeAllCities' element={<SeeAllCitiesPage />} />
-        {/* Update the route for CityDetailsPage */}
-        <Route path='/citydetails/:cityid' element={<CityDetailsPage />} />
-        {/* Add another route for CityDetailsPage with bedcount */}
-        <Route path='/citydetails/:cityid/:bedcount' element={<CityDetailsPage />} />
-        <Route path='/homedetails/:homeid' element={<HomeDetailPage />} />
-        <Route path='/favorites' element={<Favorites />} />
-      </Routes>
-      <Footer />
+      <FavContextProvider>
+        <NavBar />
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/SeeAllCities' element={<SeeAllCitiesPage />} />
+            <Route path='/citydetails/:cityid' element={<CityDetailsPage />} />
+            <Route path='/homedetails/:homeid' element={<HomeDetailPage />} />
+            <Route path='/favorites' element={<Favorites />} />
+          </Routes>
+        <Footer />
+      </FavContextProvider>
     </BrowserRouter>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,18 @@
-// NavBar.js
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdFavoriteBorder, MdMailOutline } from 'react-icons/md';
+import { FavContext } from '../../contexts/FavContext';
+import Modal from 'react-modal';
 import './NavBar.css';
 
 function NavBar() {
+  const { favorites } = useContext(FavContext);
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal((prevShowModal) => !prevShowModal);
+  };
+
   return (
     <div className='navBar-container'>
       <Link to='/' className='uniLife-logo'>
@@ -12,15 +20,25 @@ function NavBar() {
         <img src="../public/UniLife.svg" alt="UniLife" />
       </Link>
       <div className="navBar-btns">
-      <div className="navBar-btns" onClick={() => navigate('/favorites')}>
-        <MdFavoriteBorder className='navBar-icons' />
-        <p className='navBar-p'>ShortList</p>
+        <div className="navBar-btns" onClick={toggleModal}>
+          <MdFavoriteBorder className='navBar-icons' />
+          <p className='navBar-p'>ShortList</p>
+        </div>
+        <div className="navBar-btns">
+          <MdMailOutline className='navBar-icons' />
+          <p className='navBar-p'>Contact Us</p>
+        </div>
       </div>
-      <div className="navBar-btns">
-        <MdMailOutline className='navBar-icons' />
-        <p className='navBar-p'>Contact Us</p>
-      </div>
-      </div>
+
+      <Modal isOpen={showModal} onRequestClose={toggleModal} ariaHideApp={false}>
+        <h2>Favorites</h2>
+        <ul>
+          {favorites.map((property) => (
+            <li key={property.id}>{property.name}</li>
+          ))}
+        </ul>
+        <button onClick={toggleModal}>Close</button>
+      </Modal>
     </div>
   );
 }
