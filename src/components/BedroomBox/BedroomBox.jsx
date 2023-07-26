2646ebfd012e5d615fac420917927720aaae1ea9
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-07-26 10:55:00 +0200

    home details con

---
+++ src/components/BedroomBox/BedroomBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './BedroomBox.css'
+
+function BedroomBox({prices}) {
+  return (
+   <div className="bedrooms-box">
+   <h2>Bedroom Prices</h2>
+   <div>
+       {
+           prices && Object.values(prices).map( (item, index) => 
+           <div className="bed-prices" key={index}>
+               <p>{`Bedroom ${index+1}`}</p>
+               <p>{` £{item} per week`}</p>
+           </div>)         
+           
+       }
+   </div>
+</div>
+  )
+}
+
+export default BedroomBox
